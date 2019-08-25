21b0c8fe58e9cb52d1a88b048d5965f31bd96562
ttay21 <ttay21@ttay21-pc>
2019-08-25 18:11:50 -0400

    The first attempt of getting the points for the novice assignment

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,12 +3,74 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
-
+	static class Customer {
+		String firstName = "";
+		String lastName = "";
+		int items = 0;
+		int count = 0;
+		// this function is used to get the quantity of each item, 1 by 1
+		int[] quantity;  
+		void getQuantity(int amount) {
+			this.quantity[this.count] = amount;
+		};
+		void setQuantity() {
+			quantity = new int[this.items];
+			for (int i = 0; i < quantity.length; i++) {
+				this.quantity[i] = 0;
+			}
+		}
+		double[] price;
+		void setPrices() {
+			price = new double[this.items];
+			for (int i = 0; i < price.length; i++) {
+				this.price[i] = 0.0;
+			}
+		}
+		void getPrice(double price) {
+			this.price[this.count] = price;
+			this.count++;
+		};
+		double total = 0.00;
+		double getTotal () {
+			for (int i = 0; i < items; i++) {
+				this.total += (this.quantity[i]*this.price[i]);
+			}
+			return total;
+		}
+		};
+	static Customer makeCustomer() {
+	return new Customer();
+	}
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner user_input = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		int customerNum = user_input.nextInt();
+		Customer[] customer = new Customer [customerNum];
+		// this for loop is to create a new customer for each index, since
+		// I cannot figure out how to create customers from the array as the program goes on
+		for (int i = 0; i < customerNum; i++) {
+			customer[i] = makeCustomer();
+		}
+		for (int i = 0; i < customerNum; i++) {
+			customer[i].firstName = user_input.next();	
+			customer[i].lastName = user_input.next();
+			customer[i].items = user_input.nextInt();
+			customer[i].setQuantity();
+			customer[i].setPrices();
+			for (int a = 1; a <= customer[i].items; a++ ) {
+				int amount = user_input.nextInt();
+				customer[i].getQuantity(amount);
+				String itemName = user_input.next();
+				double price = user_input.nextDouble();
+				customer[i].getPrice(price);
+			}
+			}
+
+		for (int i = 0; i < customer.length; i++) {
+			System.out.println(customer[i].firstName.charAt(0) + ". " + 
+					customer[i].lastName + ": " + customer[i].getTotal());
+		}
+		user_input.close();
+		}
 	}
-}
